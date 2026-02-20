e5c9d3ff261cd49c0bb395b74dfb02876cd8d286
Joachim Reichel <43646584+jreichel-nvidia@users.noreply.github.com>
2026-02-20 09:28:00 +0100

    Update copyright year to 2026

--- src/mdl/jit/generator_jit/gen_libbsdf_data_tables.py
+++ src/mdl/jit/generator_jit/gen_libbsdf_data_tables.py
@@ -1,6 +1,6 @@
 #!/usr/bin/env python
 #*****************************************************************************
-# Copyright (c) 2024-2025, NVIDIA CORPORATION. All rights reserved.
+# Copyright (c) 2024-2026, NVIDIA CORPORATION. All rights reserved.
 #
 # Redistribution and use in source and binary forms, with or without
 # modification, are permitted provided that the following conditions
@@ -34,7 +34,7 @@ import os
 import struct
 
 copyright_str = """/******************************************************************************
- * Copyright (c) 2024-2025, NVIDIA CORPORATION. All rights reserved.
+ * Copyright (c) 2024-2026, NVIDIA CORPORATION. All rights reserved.
  *
  * Redistribution and use in source and binary forms, with or without
  * modification, are permitted provided that the following conditions
